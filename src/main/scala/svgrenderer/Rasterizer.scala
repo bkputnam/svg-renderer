cc17ff06a1f1d4bb2c928679ca141ea7da1f7bc9
Brian Putnam <brian.putnam@lasp.colorado.edu>
2015-03-03 19:44:01 -0700

    basic Batik renderer working

---
+++ src/main/scala/svgrenderer/Rasterizer.scala
@@ -0,0 +1,155 @@
+package svgrenderer
+
+import java.io.OutputStream
+import org.apache.batik.transcoder.{TranscoderInput, TranscoderOutput}
+import org.apache.batik.transcoder.Transcoder
+import org.apache.batik.gvt.renderer.ImageRenderer
+import org.apache.batik.transcoder.image.PNGTranscoder
+import java.awt.RenderingHints
+import org.apache.batik.transcoder.image.JPEGTranscoder
+import org.apache.batik.transcoder.svg2svg.SVGTranscoder
+import org.apache.fop.svg.PDFTranscoder
+import java.io.ByteArrayOutputStream
+import java.io.ByteArrayInputStream
+import java.nio.charset.StandardCharsets
+import org.apache.batik.transcoder.TranscodingHints
+import org.apache.batik.transcoder.SVGAbstractTranscoder
+import java.io.InputStream
+
+object Rasterizer {
+  
+  /**
+   * Given a string containing SVG and a target mime type,
+   * return a byte array representing the rasterized
+   * (rendered to bitmap-like form) image
+   */
+  def rasterize(svgStr: String, mimeType: String, width: Option[Int]): Array[Byte] = {
+    
+    // Adapted from: http://stackoverflow.com/questions/11234455/poor-image-quality-when-using-svgconverter-in-batik-to-convert-svg-to-png
+    
+    val inputStream: InputStream = new ByteArrayInputStream(svgStr.getBytes(StandardCharsets.UTF_8))
+    val transcoderInput: TranscoderInput = new TranscoderInput(inputStream)
+    transcoderInput.setURI("http://example.com")
+    val output: ByteArrayOutputStream = new ByteArrayOutputStream
+    val transcoderOutput: TranscoderOutput = new TranscoderOutput(output)
+    
+    val transcoder: Transcoder = mimeType match {
+      case "image/png" => getPngTranscoder
+      case "image/jpeg" => getJpegTranscoder
+      case "application/pdf" => getPdfTranscoder
+      case "image/svg+xml" => getSvgTranscoder
+      case _ => throw new Exception(s"Unknown mime type: '$mimeType'")
+    }
+    
+    if (!width.isEmpty) {
+      transcoder.addTranscodingHint(SVGAbstractTranscoder.KEY_WIDTH, width.get)
+    }
+    
+    try {
+      transcoder.transcode(transcoderInput, transcoderOutput)
+    } catch {
+      case e: Exception => {
+        if(output != null) {
+          output.close()
+        }
+        throw e
+      }
+    }
+    
+    if (output != null) {
+      output.flush()
+      output.close()
+    }
+    
+    return output.toByteArray()
+  }
+  
+  /**
+   * Create a PNG Transcoder
+   * 
+   * Mostly just a place to configure a PNG Transcoder that's
+   * away from the main flow of code
+   */
+  private def getPngTranscoder: Transcoder = {
+    new CustomPngTranscoder
+  }
+  
+  /**
+   * Create a JPEG Transcoder
+   * 
+   * Mostly just a place to configure a JPEG Transcoder that's
+   * away from the main flow of code
+   */
+  private def getJpegTranscoder: Transcoder = {
+    // Copied from: http://stackoverflow.com/questions/11234455/poor-image-quality-when-using-svgconverter-in-batik-to-convert-svg-to-png
+    val result = new JPEGTranscoder
+    val jpegQuality: Float = 0.95f
+    result.addTranscodingHint(JPEGTranscoder.KEY_QUALITY, jpegQuality)
+    return result
+  }
+  
+  /**
+   * Create a PDF Transcoder
+   * 
+   * Mostly just a place to configure a PDF Transcoder that's
+   * away from the main flow of code
+   */
+  private def getPdfTranscoder: Transcoder = new PDFTranscoder
+  
+  /**
+   * Create a SVG Transcoder
+   * 
+   * Mostly just a place to configure a SVG Transcoder that's
+   * away from the main flow of code
+   * 
+   * Ps - I have no idea why you'd want to transcode SVG to SVG,
+   * but it's an option in Highcharts' PHP rasterizer, so I
+   * made sure it was also an option here.
+   * http://www.highcharts.com/docs/export-module/setting-up-the-server
+   * https://github.com/highslide-software/highcharts.com/blob/master/exporting-server/php/php-batik/index.php
+   */
+  private def getSvgTranscoder: Transcoder = new SVGTranscoder
+  
+  /**
+   * Quick, private custom PNGTranscoder class, so that we can
+   * override the createRenderer method.
+   */
+  private class CustomPngTranscoder extends PNGTranscoder {
+    
+    override def createRenderer: ImageRenderer = {
+      
+      // Copied from: http://stackoverflow.com/questions/11234455/poor-image-quality-when-using-svgconverter-in-batik-to-convert-svg-to-png
+      val r: ImageRenderer = super.createRenderer()
+      
+      val rh: RenderingHints = r.getRenderingHints
+      
+      rh.add(new RenderingHints(RenderingHints.KEY_ALPHA_INTERPOLATION,
+        RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY));
+      rh.add(new RenderingHints(RenderingHints.KEY_INTERPOLATION,
+        RenderingHints.VALUE_INTERPOLATION_BICUBIC));
+      
+      rh.add(new RenderingHints(RenderingHints.KEY_ANTIALIASING,
+        RenderingHints.VALUE_ANTIALIAS_ON));
+      
+      rh.add(new RenderingHints(RenderingHints.KEY_COLOR_RENDERING,
+        RenderingHints.VALUE_COLOR_RENDER_QUALITY));
+      rh.add(new RenderingHints(RenderingHints.KEY_DITHERING,
+        RenderingHints.VALUE_DITHER_DISABLE));
+      
+      rh.add(new RenderingHints(RenderingHints.KEY_RENDERING,
+        RenderingHints.VALUE_RENDER_QUALITY));
+      
+      rh.add(new RenderingHints(RenderingHints.KEY_STROKE_CONTROL,
+        RenderingHints.VALUE_STROKE_PURE));
+      
+      rh.add(new RenderingHints(RenderingHints.KEY_FRACTIONALMETRICS,
+        RenderingHints.VALUE_FRACTIONALMETRICS_ON));
+      rh.add(new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING,
+        RenderingHints.VALUE_TEXT_ANTIALIAS_OFF));
+      
+      r.setRenderingHints(rh);
+      return r
+    }
+  }
+
+}
--- src/main/scala/svgrenderer/WebServer.scala
+++ src/main/scala/svgrenderer/WebServer.scala
@@ -1,14 +1,60 @@
 package svgrenderer
 
 import javax.servlet.http.{ HttpServlet, HttpServletRequest, HttpServletResponse }
+import javax.servlet.annotation.MultipartConfig
+import scala.util.matching.Regex
+import org.apache.batik.script.rhino.BatikSecurityController
+import java.io.ByteArrayOutputStream
+import java.io.DataInputStream
 
+@MultipartConfig(location="/tmp",
+    fileSizeThreshold=20*1024*1024, // 20 MB - The size threshold after which the file will be written to disk
+    maxFileSize=10*1024*1024, // 10 MB - The maximum size allowed for uploaded files
+    maxRequestSize=20*1024*1024) // 20 MB - The maximum size allowed for multipart/form-data requests
 class WebServer extends HttpServlet {
   
-  override def init() {
-    /* maybe do stuff in the future? */
-  }
+  private val companion = WebServer
   
-  override def doGet(request: HttpServletRequest, response: HttpServletResponse) {
-    response.getOutputStream.print("hello world")
+  /**
+   * Handle every request coming into this server. This server
+   * will take in SVG and return a rasterized (rendered to
+   * a bitmap-style image) version of it.
+   * 
+   * This code delegates to Apache Batik under the hood
+   * 
+   * This code was ported from Highcharts' PHP script that does
+   * the same:
+   * https://github.com/highslide-software/highcharts.com/blob/master/exporting-server/php/php-batik/index.php 
+   */
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse) {
+    
+    val requestedMimeType = request.getParameter("type")
+    
+    // turning an uploaded file into a string is harder than it should be...
+    val svgPart = request.getPart("svg")
+    val numBytes: Int = svgPart.getSize.toInt
+    val svgBytes = new Array[Byte](numBytes)
+    (new DataInputStream(svgPart.getInputStream)).readFully(svgBytes)
+    val svg: String = new String(svgBytes)
+    
+    val filename = request.getParameter("filename") match {
+      case companion.FILENAME_REGEX(fname) => fname
+      case _ => "chart"
+    }
+    val width: Option[Int] = try { Some(request.getParameter("width").toInt) } catch { case _: Throwable => None }
+    
+    // check for malicious attack in SVG
+    if(svg.indexOfSlice("<!ENTITY") > -1 || svg.indexOfSlice("<!DOCTYPE") > -1) {
+      throw new Exception("Exception is stopped, the posted SVG could contain code for a malicious attack")
+    }
+    
+    val rasterizedImage: Array[Byte] = Rasterizer.rasterize(svg, requestedMimeType, width)
+    
+    response.setContentType(requestedMimeType)
+    response.getOutputStream.write(rasterizedImage)
   }
+}
+
+object WebServer {
+  private val FILENAME_REGEX: Regex = """(^[A-Za-z0-9\-_ ]+$)""".r
 }
